1573de4c80fffe48a465bffa6deda4a2acc035e3
iovxw <iovxw@outlook.com>
2019-07-19 22:50:39 +0800

    Update i18n

---
+++ gleipnir/assets/i18n/base.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FirewallPage</name>
+    <message>
+        <location filename="../FirewallPage.qml" line="55"/>
+        <source>Direction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="68"/>
+        <source>Proto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="80"/>
+        <source>Program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="91"/>
+        <source>Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="102"/>
+        <source>Port Range</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="113"/>
+        <source>Target</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="194"/>
+        <location filename="../FirewallPage.qml" line="201"/>
+        <source>Any</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="194"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="194"/>
+        <source>Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="219"/>
+        <source>Please choose a program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="360"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="360"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="416"/>
+        <source>Default Target: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="424"/>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="427"/>
+        <source>Drop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="463"/>
+        <source>Rate Limit Rules</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="468"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FirewallPage.qml" line="478"/>
+        <source>Illegal rules:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MonitorPage</name>
+    <message>
+        <location filename="../MonitorPage.qml" line="209"/>
+        <source>Default Rule</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RateLimitRulesPopup</name>
+    <message>
+        <location filename="../RateLimitRulesPopup.qml" line="43"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../RateLimitRulesPopup.qml" line="57"/>
+        <source>Limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="9"/>
+        <source>Gleipnir</source>
+        <extracomment>Window title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="41"/>
+        <source>Monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="44"/>
+        <source>Firewall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="94"/>
+        <source>Daemon not found, start it manually?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="98"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="101"/>
+        <source>Failed to start daemon:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="138"/>
+        <source>Error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
