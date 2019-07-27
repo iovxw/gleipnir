dc6ebe28a85657c8b9e6ecce3d8b3f19a70db4c0
iovxw <iovxw@outlook.com>
2019-07-27 23:08:32 +0800

    UI: Add Chinese translation

--- gleipnir/assets/i18n/zh_CN.ts
+++ gleipnir/assets/i18n/zh_CN.ts
@@ -1,98 +1,98 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
 <context>
     <name>FirewallPage</name>
     <message>
         <location filename="../FirewallPage.qml" line="55"/>
         <source>Direction</source>
-        <translation type="unfinished"></translation>
+        <translation>方向</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="68"/>
         <source>Proto</source>
-        <translation type="unfinished"></translation>
+        <translation>协议</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="80"/>
         <source>Program</source>
-        <translation type="unfinished"></translation>
+        <translation>程序</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="91"/>
         <source>Address</source>
-        <translation type="unfinished"></translation>
+        <translation>地址</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="102"/>
         <source>Port Range</source>
-        <translation type="unfinished"></translation>
+        <translation>端口范围</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="113"/>
         <source>Target</source>
-        <translation type="unfinished"></translation>
+        <translation>规则</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="194"/>
         <location filename="../FirewallPage.qml" line="201"/>
         <source>Any</source>
-        <translation type="unfinished"></translation>
+        <translation>任意</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="194"/>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation>入站</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="194"/>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation>出站</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="219"/>
         <source>Please choose a program</source>
-        <translation type="unfinished"></translation>
+        <translation>请选择一个程序</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="360"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>确认</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="360"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>删除</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="416"/>
         <source>Default Target: </source>
-        <translation type="unfinished"></translation>
+        <translation>默认规则: </translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="424"/>
         <source>Accept</source>
-        <translation type="unfinished"></translation>
+        <translation>接受</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="427"/>
         <source>Drop</source>
-        <translation type="unfinished"></translation>
+        <translation>丢弃</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="463"/>
         <source>Rate Limit Rules</source>
-        <translation type="unfinished"></translation>
+        <translation>速率限制</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="468"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>应用</translation>
     </message>
     <message>
         <location filename="../FirewallPage.qml" line="478"/>
         <source>Illegal rules:</source>
-        <translation type="unfinished"></translation>
+        <translation>非法规则:</translation>
     </message>
 </context>
 <context>
@@ -100,7 +100,7 @@
     <message>
         <location filename="../MonitorPage.qml" line="209"/>
         <source>Default Rule</source>
-        <translation type="unfinished"></translation>
+        <translation>默认规则</translation>
     </message>
 </context>
 <context>
@@ -108,12 +108,12 @@
     <message>
         <location filename="../RateLimitRulesPopup.qml" line="43"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>名字</translation>
     </message>
     <message>
         <location filename="../RateLimitRulesPopup.qml" line="57"/>
         <source>Limit</source>
-        <translation type="unfinished"></translation>
+        <translation>限制</translation>
     </message>
 </context>
 <context>
@@ -122,37 +122,38 @@
         <location filename="../main.qml" line="9"/>
         <source>Gleipnir</source>
         <extracomment>Window title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Gleipnir</translation>
     </message>
     <message>
         <location filename="../main.qml" line="41"/>
         <source>Monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>监视器</translation>
     </message>
     <message>
         <location filename="../main.qml" line="44"/>
         <source>Firewall</source>
-        <translation type="unfinished"></translation>
+        <translation>防火墙</translation>
     </message>
     <message>
         <location filename="../main.qml" line="94"/>
         <source>Daemon not found, start it manually?</source>
-        <translation type="unfinished"></translation>
+        <translation>未找到
+守护进程，是否手动启动？</translation>
     </message>
     <message>
         <location filename="../main.qml" line="98"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>是</translation>
     </message>
     <message>
         <location filename="../main.qml" line="101"/>
         <source>Failed to start daemon:</source>
-        <translation type="unfinished"></translation>
+        <translation>启动守护进程失败：</translation>
     </message>
     <message>
         <location filename="../main.qml" line="138"/>
         <source>Error!</source>
-        <translation type="unfinished"></translation>
+        <translation>错误！</translation>
     </message>
 </context>
 </TS>
