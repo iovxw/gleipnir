adb6e95f01c23756d156c6f064a8c32472a7c6a6
iovxw <iovxw@outlook.com>
2019-10-07 00:40:19 +0800

    Upgrade dbus-rs to 0.4

--- gleipnird/src/polkit.rs
+++ gleipnird/src/polkit.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use std::fs;
+use std::time::Duration;
 
 use dbus::arg::{RefArg, Variant};
-use dbus::{BusType, Connection};
+use dbus::blocking::Connection;
 
 include!(concat!(env!("OUT_DIR"), "/dbus_interfaces.rs"));
 
@@ -10,13 +11,14 @@ pub fn check_authorization(pid: u32) -> bool {
     let stat = fs::read_to_string(format!("/proc/{}/stat", pid)).expect("invalid pid");
     let start_time: u64 = stat.split(' ').skip(21).next().unwrap().parse().unwrap();
 
-    let conn = Connection::get_private(BusType::System).expect("connect to dbus");
+    let conn = Connection::new_system().expect("connect to dbus");
 
-    let authority = conn.with_path(
+    let authority = conn.with_proxy(
         "org.freedesktop.PolicyKit1",
         "/org/freedesktop/PolicyKit1/Authority",
-        100000,
+        Duration::from_secs(1),
     );
+
     let mut subject: HashMap<&str, Variant<Box<dyn RefArg>>> = HashMap::new();
     subject.insert("pid", Variant(Box::new(pid)));
     subject.insert("start-time", Variant(Box::new(start_time)));
